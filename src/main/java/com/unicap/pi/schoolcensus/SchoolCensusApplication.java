1454a4de16db310ee989701c6c4f5842de765875
luizgnclvs <luizgnclvss@gmail.com>
2023-12-02 15:00:47 -0300

    added spring project

---
+++ src/main/java/com/unicap/pi/schoolcensus/SchoolCensusApplication.java
@@ -0,0 +1,13 @@
+package com.unicap.pi.schoolcensus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCensusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCensusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicap/pi/schoolcensus/SchoolCensusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicap.pi.schoolcensus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCensusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
