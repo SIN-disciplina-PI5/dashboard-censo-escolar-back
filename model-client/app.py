3342a4a892886468a68b7a8b9e33f9f649b82e3e
luizgnclvs <luizgnclvss@gmail.com>
2023-12-02 16:40:44 -0300

    updated model path

--- model-client/app.py
+++ model-client/app.py
@@ -1,12 +1,17 @@
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
-model = pickle.load(open('./model-client/model.sav', 'rb'))
+if (os.path.isfile('./model.sav')):
+    path = './model.sav'
+else:
+    path = './model-client/model.sav'
+
+model = pickle.load(open(path, 'rb'))
 
 def orderData(data):
     return [data[param] for param in ['region-code', 'uf-code', 'county-code', 'meso-region-code', 'micro-region-code', 'district-code', 'dependency-type', 'location-type', 'differentiated-location-type', 'education-department-link', 'public-security-link', 'health-department-link', 'other-department-link']]
