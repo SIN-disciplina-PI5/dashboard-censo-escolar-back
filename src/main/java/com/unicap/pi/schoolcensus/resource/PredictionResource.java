61946a47d4e723b8b94a11d40836b3643f921f50
luizgnclvs <luizgnclvss@gmail.com>
2023-12-11 23:51:31 -0300

    feat: added suport for cross origins

--- src/main/java/com/unicap/pi/schoolcensus/resource/PredictionResource.java
+++ src/main/java/com/unicap/pi/schoolcensus/resource/PredictionResource.java
@@ -6,14 +6,12 @@ import com.unicap.pi.schoolcensus.service.PredictionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/predict")
 @RequiredArgsConstructor
+@CrossOrigin
 public class PredictionResource {
 
     private final PredictionService predictionService;
